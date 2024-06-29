7b10d81c5e323ac72f6c0a7ebcaeea1e98a32a88
Shunsuke Ohashi <amylase.inquiry@gmail.com>
2024-06-29 19:50:11 +0900

    base64 integer

--- amylase/base64.py
+++ amylase/base64.py
@@ -5,29 +5,28 @@ import argparse
 def compress(to_encode: str) -> str:
     chars = "LRDU"
 
-    for unit_size in range(8, 1, -1):
-        candidate_units = [''.join(t) for t in itertools.product(chars, repeat=unit_size)]
+    unit_size = 8
+    candidate_units = [''.join(t) for t in itertools.product(chars, repeat=unit_size)]
 
-        indices = []
-        for i in range(0, len(to_encode), unit_size):
-            raw = to_encode[i: i + unit_size]
-            for v, unit in enumerate(candidate_units):
-                if unit[: len(raw)] == raw:
-                    indices.append(v)
-                    break
-        if len(set(indices)) > 94:
-            continue
-            # raise ValueError(f"failed to construct lookup table for unit_size {unit_size}")
-        break
+    indices = []
+    for i in range(0, len(to_encode), unit_size):
+        raw = to_encode[i: i + unit_size]
+        for v, unit in enumerate(candidate_units):
+            if unit[: len(raw)] == raw:
+                indices.append(v)
+                break
        
     zahyo_assyuku = {v: i for i, v in enumerate(set(indices))}
     zahyo_tenkai = {i: v for v, i in zahyo_assyuku.items()}
 
     units = [candidate_units[zahyo_tenkai[i]] for i in range(len(zahyo_assyuku))]
     indices = [zahyo_assyuku[v] for v in indices]
+    base = len(units)
 
     assert ''.join([units[v] for v in indices])[:len(to_encode)] == to_encode
-    encoded = ''.join([chr(v + 33) for v in indices])
+    encoded = base
+    for v in reversed(indices):
+        encoded = encoded * base + v
 
     table = ''.join(units)
 
@@ -50,14 +49,14 @@ B$
   -- Decode :: Self -> String -> String
   -- Decodes the first entry in the source then recursively process the rest.
   Ld Ls
-    ? ( B= vs S )
+    ? ( B= vs @I{base} )
       S
       B.
-        BT @I{unit_size} BD (B* @I{unit_size} U# (BT @I1 vs)) @S{table} -- Decode the first entry
-        (B$ vd (BD @I1 vs)) -- Process the rest
+        BT @I{unit_size} BD (B* @I{unit_size} B% vs @I{base}) @S{table} -- Decode the first entry
+        (B$ vd (B/ vs @I{base})) -- Process the rest
 
 -- Encoded string
-S{encoded}
+@I{encoded}
 """
     return source_code
 
