b26d7fec5eee5c433e365a8179d5b480f671317e
Shunsuke Ohashi <amylase.inquiry@gmail.com>
2024-06-30 23:01:27 +0900

    random walk that solves ramudaman

---
+++ amylase/randomwalk.py
@@ -0,0 +1,28 @@
+# this output solves lambdaman5 (roll cake)
+
+import random
+import io
+
+
+class RNG:
+    def __init__(self, initial_state: int) -> None:
+        self.state = initial_state
+    
+    def get_integer(self) -> int:
+        self.state = 48271 * self.state % ((2 ** 31) - 1)
+        return self.state
+
+
+def main():
+    rng = random.Random()
+    rng = RNG(11111)
+    buf = io.StringIO()
+    for _ in range(200000):
+        d = "UDRL"[rng.get_integer() % 4]
+        l = (rng.get_integer() % 4 + 1) * 2
+        buf.write(d * l)
+    print(buf.getvalue())
+
+
+if __name__ == "__main__":
+    main()
