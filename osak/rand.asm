706319a19da85437ad63cb5e1353854830f9d850
Osamu Koga <github@osak.jp>
2024-06-30 23:45:53 +0900

    random walk

---
+++ osak/rand.asm
@@ -0,0 +1,16 @@
+B. @Ssolve@_lambdaman17@_
+
+B$ B$ -- Initial parameters
+-- Make recursive Solve
+B$
+Lf B$ Lx B$ vf B$ vx vx Lx B$ vf B$ vx vx -- Fix
+-- Solve :: Self -> Int -> Int -> String
+Ls Ln Lp
+    ? (B= vn @I0)
+    S
+    B$ L1 -- rand1
+      B.
+        BT @I1 BD (B% v1 @I4) @SUDRL 
+        B$ B$ vs (B- vn @I1) v1
+    B% (B* @I48271 vp) @I2147483647
+@I900000 @I11111
