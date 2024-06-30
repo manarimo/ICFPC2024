032d256903aa5dcb73c8b7983dd0555b6f343ecf
Osamu Koga <github@osak.jp>
2024-07-01 01:59:56 +0900

    remove counter

--- osak/rand.asm
+++ osak/rand.asm
@@ -1,14 +1,15 @@
 B. @Ssolve@_lambdaman10@_
 
+-- Make recursive Solve
 B$
 Lf
-    B$ B$ B$ vf vf @I800000 @I1111
--- Solve :: Self -> Int -> Int -> String
-Ls Ln Lp
-    ? (B= vn @I0)
+    B$ B$ vf vf @I11111
+-- Solve :: Self -> Int -> String
+Ls Lp
+    ? (B= vp @I171613852) -- RNG value at 900000th iteration
     S
     B$ L1 -- rand1
       B.
         BT @I1 BD (B% v1 @I4) @SUDRL 
-        B$ B$ B$ vs vs (B- vn @I1) v1
-    B% (B* @I48271 vp) @I78074891
+        B$ B$ vs vs v1
+    B% (B* @I48271 vp) @I2147483647
