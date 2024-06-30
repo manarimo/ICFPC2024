344d41972d44b0f8df7bca8941ddaeb0a6deb259
Shunsuke Ohashi <amylase.inquiry@gmail.com>
2024-06-30 18:40:05 +0900

    pattern solver

--- lambdaman-web/src/App.tsx
+++ lambdaman-web/src/App.tsx
@@ -4,11 +4,11 @@ import LambdamanSimulator from './LambdamanSimulator'
 
 function App() {
   const [problem, setProblem] = useState("###.#...\n...L..##\n.#######")
-  const [solution, setSolution] = useState("LLLDURRRUDRRURR")
 
-  const [patterns, setPatterns] = useState([])
-  const [moves, setMoves] = useState("")
+  const [patterns, setPatterns] = useState(["LLLDURRRUDRRURR"])
+  const [moves, setMoves] = useState("0")
 
+  const solution = moves.split('').map((move) => patterns[parseInt(move)]).join('')  
   const width = 80
   return (
     <>
@@ -17,7 +17,33 @@ function App() {
       </div>
 
       <div>
-        <input type='text' value={solution} size={width} onChange={(e) => setSolution(e.target.value)}/>
+        <input type='text' value={solution} size={width} readOnly />
+      </div>
+
+      <div>
+        <div>
+          {patterns.map((pattern, i) => {
+            return (
+              <div key={`pattern-${i}`}>
+                <span>Pattern {i}: </span>
+                <input type='text' value={pattern} onChange={(e) => { 
+                  const newPatterns = patterns.concat()
+                  newPatterns[i] = e.target.value
+                  setPatterns(newPatterns) 
+                }}/>
+                <button type='button' onClick={() => setPatterns(patterns.slice(0, i).concat(...patterns.slice(i + 1))) }>del</button>
+              </div>
+            )
+          })}
+        </div>
+        <div>
+          <button type='button' onClick={() => setPatterns(patterns.concat("")) }>add</button>
+        </div>
+      </div>
+
+      <div>
+      <span>Moves: </span>
+      <input type='text' value={moves} onChange={(e) => setMoves(e.target.value)} />
       </div>
 
       <div>
