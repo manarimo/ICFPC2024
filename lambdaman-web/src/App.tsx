77d6246abbb70d48a5e4220477cf3caf56aeb888
Shunsuke Ohashi <amylase.inquiry@gmail.com>
2024-06-30 18:03:09 +0900

    something working

--- lambdaman-web/src/App.tsx
+++ lambdaman-web/src/App.tsx
@@ -1,33 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import LambdamanSimulator from './LambdamanSimulator'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [problem, setProblem] = useState("###.#...\n...L..##\n.#######")
+  const [solution, setSolution] = useState("LLLDURRRUDRRURR")
 
+  const width = 80
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <textarea value={problem} cols={width} rows={10} onChange={(e) => setProblem(e.target.value)}/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      <div>
+        <input type='text' value={solution} size={width} onChange={(e) => setSolution(e.target.value)}/>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <LambdamanSimulator problem={problem} solution={solution}/>
     </>
   )
 }
