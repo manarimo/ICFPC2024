46c55320a532d88213f41c5913d4a898f970ce82
Shunsuke Ohashi <amylase.inquiry@gmail.com>
2024-07-01 20:35:18 +0900

    12.asm

---
+++ lambdaman/ans/12.asm
@@ -0,0 +1,15 @@
+-- Make recursive Solve
+B$
+Lf
+    B$ B$ vf vf @I74
+-- Solve :: Self -> Int -> String
+Ls Lp
+    ? (B= vp @I1)
+    @Ssolve@_lambdaman12@_
+    B.
+      B$ B$ vs vs (B% (B* @I11 vp) @I990893)
+      B. ? (B= vp @I456670) @SLLLLRRDDUURR S
+      B. ? (B= vp @I349582) @SDDLLRRUU S
+      B. ? (B= vp @I177436) @SLLRR S
+      B. ? (B= vp @I762149) @SUURRUUDDLLDD S
+      BT @I2 BD B* @I2 (B% vp @I4) @SUUDDRRLL
